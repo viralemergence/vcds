93d6758beaf025d5ef8e17737f52e3b0f744a71a
collinschwantes <collin@viralemergence.org>
2026-09-25 14:07:16 -0400

    adding taxon id

--- scripts/increment_version.R
+++ scripts/increment_version.R
@@ -6,4 +6,4 @@ source("R/increment_version.R")
 ## a new major version is released.
 
 
-increment_schema_version(path = "vcds_schema/v1.0.0/vcds_schema.json",increment = "patch")
+increment_schema_version(path = "vcds_schema/v1.1.0/vcds_schema.json",increment = "patch")
